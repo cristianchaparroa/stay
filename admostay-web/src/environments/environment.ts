0bbcd9b28f1567b8dcd0f81bc3e449dfa6a3f3de
cristianchaparroa <cristianchaparroa@gmail.com>
2022-12-19 21:21:31 -0500

    remove credentials

--- admostay-web/src/environments/environment.ts
+++ admostay-web/src/environments/environment.ts
@@ -4,15 +4,6 @@
 
 export const environment = {
   production: false,
-  firebaseConfig: {
-    apiKey: "AIzaSyB9QO01D80_2TrGcl-ImU8DTfLqO8bi9wk",
-    authDomain: "stay-5c81e.firebaseapp.com",
-    projectId: "stay-5c81e",
-    storageBucket: "stay-5c81e.appspot.com",
-    messagingSenderId: "126343230844",
-    appId: "1:126343230844:web:ff7c679c1b003d397660f9",
-    databaseURL: "https://stay-5c81e-default-rtdb.firebaseio.com",
-  }
 };
 
 /*
