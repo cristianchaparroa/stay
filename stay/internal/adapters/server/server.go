9896217ae6609d29ed6212a6ec8716e020247884
Cristian <cristianchaparroa@users.noreply.github.com>
2022-12-20 23:26:14 -0500

    feature: setup the server (#3)

---
+++ stay/internal/adapters/server/server.go
@@ -0,0 +1,24 @@
+package server
+
+import (
+	"fmt"
+	"stay/internal/app"
+
+	"github.com/labstack/echo/v4"
+)
+
+
+type Server struct {
+    e *echo.Echo
+    config *app.Config
+}
+
+func New(conf  *app.Config) *Server {
+    e := echo.New()
+    return &Server{e: e, config: conf}
+}
+
+func (s *Server) Run() {
+    s.e.Start(fmt.Sprintf(":%d", s.config.Port))
+}
+    
