763a0a6cb47cac0e023d36c5aa8d556a6abcf9f9
cristianchaparroa <cristianchaparroa@gmail.com>
2022-12-23 17:27:55 -0500

    create the base structs for server side and add ci pipeline

---
+++ stay/internal/ports/property.go
@@ -0,0 +1,11 @@
+package ports
+
+// Property represents the space rent on platforms.
+type Property struct {
+	ID          string
+	Name        string
+	Description string
+	Address     string
+	Typ         string
+	Rooms       int
+}
